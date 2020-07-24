8ee477312f17781b6ab1d3255b7973dac782e431
GAOGA4 <961977096@qq.com>
2020-07-24 09:09:19 +0800

    [Gavin] refactor:refactor PlayGuessNumberGame

--- src/main/java/example/PlayGuessNumberGame.java
+++ src/main/java/example/PlayGuessNumberGame.java
@@ -4,31 +4,43 @@ import java.util.Scanner;
 
 public class PlayGuessNumberGame {
 
-
-        public void playGuessNumberGame(){
-
-
-        GenerateRightFormatAnswer generateRightFormatAnswer = new GenerateRightFormatAnswer();
-
-        int count = 1;
-        String answer = generateRightFormatAnswer.generateAnswer();
+    public String getConsoleInput(){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("四位数字答案已生成，请输入你要猜的数字：");
         String guessNum = scanner.next().replace(" ", "");
+        return guessNum;
+    }
+    public String GenerateAnswer(){
+        GenerateRightFormatAnswer generateRightFormatAnswer = new GenerateRightFormatAnswer();
+        String answer = generateRightFormatAnswer.generateAnswer();
+        System.out.println("四位数字答案已生成，请输入你要猜的数字：");
+        return  answer;
+    }
+    public String filterNumber(String number){
+        NumberFilter numberFilter = new NumberFilter(number);
+        String output = numberFilter.filter();
+        return output;
+    }
+    public String guess(String guessNum,String answerNum){
+        GuessNumberGame guessNumberGame = new GuessNumberGame(answerNum);
+        String result = guessNumberGame.guess(guessNum);
+        return result;
+    }
+    public void playGuessNumberGame() {
+        String guessNum=getConsoleInput();
+        int count = 0;
         while (count < 6) {
-            count++;
-            NumberFilter numberFilter = new NumberFilter(guessNum);
-            String output = numberFilter.filter();
+            String answer=GenerateAnswer();
+            String output = filterNumber(guessNum);
             if (output.equals("Pass")) {
-                GuessNumberGame guessNumberGame = new GuessNumberGame(guessNum);
-                String result = guessNumberGame.guess(answer);
+                String result = guess(guessNum,answer);
                 System.out.println(result);
             } else {
                 System.out.println(output);
-                guessNum = scanner.next().replace(" ", "");
+                guessNum = getConsoleInput();
             }
+            count++;
         }
         System.out.println("机会用完");
-        }
+    }
 
 }
